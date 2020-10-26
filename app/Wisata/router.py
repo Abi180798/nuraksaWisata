15a2ed4a8f5d1d318eee09b8a255c02f2d303d06
Abi180798 <habiburrahman180798@gmail.com>
2020-10-26 15:26:27 +0800

    add service upload images

--- app/Wisata/router.py
+++ app/Wisata/router.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, File, UploadFile
 from app.response import *
 from .views import WisataViewControl
+from .models import Wisata,session
 from .schemas import *
 from app.Auth.auth import get_current_superadmin, get_current_admin
+
+import os, io, cv2
+from starlette.responses import StreamingResponse
+
+ALOWRD_FILE = ['png', 'jpg', 'jpeg']
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+FOTO_DIR = os.path.join(BASE_DIR, "images")
 
 wisataRouter = APIRouter()
 viewControl = WisataViewControl()
@@ -44,3 +52,49 @@ async def deleteById(
   authorized = Depends(get_current_admin)
   ):
   return httpResponse(viewControl.delete,res=res,id=id) 
+
+
+@wisataRouter.post("/wisatas/photo", status_code=status.HTTP_201_CREATED, response_model=WisataResponse)
+async def uploadPhoto( 
+    res : Response,
+    id : int,
+    photo :UploadFile = File(...)
+    ):
+    response = WisataResponse()
+    wisata : Wisata = Wisata.getWisataBy(Wisata.id_wisata==id)
+    print(wisata)
+    if wisata is None:
+        return response.notfound()
+    # photo.file.
+    try:
+        img_path = os.path.join(FOTO_DIR, photo.filename)
+        file_format = img_path.split(".")[1]
+        if not file_format in ALOWRD_FILE:
+            res.status_code = status.HTTP_400_BAD_REQUEST
+            response.badrequest()
+            return response
+        with open(img_path, 'wb') as f:
+            [f.write(chunk) for chunk in iter(lambda: photo.file.read(10000), b'')]
+
+        wisata.gambar_wisata = photo.filename
+        session.commit()
+        response.created()
+        response.data = wisata
+        return response
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="internal server error")
+
+@wisataRouter.get("/wisatas/photo/{photo_name}", status_code = status.HTTP_200_OK)
+async def getPhot(
+    res : Response,
+    photo_name : str
+    ):
+    try:
+        image = cv2.imread(os.path.join(FOTO_DIR, photo_name))
+        if image is None:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="image not found")
+        file_format = photo_name.split(".")[1]
+        res, img = cv2.imencode(".{}".format(file_format), image)
+        return StreamingResponse(io.BytesIO(img.tobytes()), media_type="image/{}".format(file_format))
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
